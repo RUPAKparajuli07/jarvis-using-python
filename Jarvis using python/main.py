10f5a8e917e5cd7e5cc92d16cbee8ab1785a3cd0
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-07-25 19:12:11 +0545

    Update main.py

--- Jarvis using python/main.py
+++ Jarvis using python/main.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
 
         elif "reset chat".lower() in query.lower():
             chatStr = ""
-
+            
         else:
             print("Chatting...")
             chat(query)
